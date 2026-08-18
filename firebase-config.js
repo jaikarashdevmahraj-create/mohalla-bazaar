ae1c208664079a149b5388a982ef03e909750594
jaikarashdevmahraj-create <jaikarashdevmahraj@gmail.com>
2026-08-18 19:55:01 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,15 @@
 // ===== Firebase से जोड़ना =====
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {
+  getFirestore,
+import {
   getFirestore,
   collection,
   doc,
